24575857d3f79d687a73c6209ab693671a63f79c
Lilian Gallon <lilian.gallon@etu.u-bordeaux.fr>
2018-08-30 17:13:00 +0200

    add scroll to list

---
+++ py/retrieve_runes_list.py
@@ -0,0 +1,20 @@
+import os
+from pathlib import Path
+
+directory = "images/runes"
+runes = list(Path(directory).rglob("rune_*.png"))
+
+for rune in runes:
+    rune_name = os.path.basename(rune).replace("_", " ").replace(".png", "")
+    
+    category = "simple"
+    if "rune pa" in rune_name:
+        category = "pa"
+    elif "rune ra" in rune_name:
+        category = "ra"
+
+    line = '{ label: "' + rune_name + \
+           '", category: "' + category + \
+           '", icon: "/images/runes/' + category + "/" + os.path.basename(rune) + '"},'
+
+    print(line)
