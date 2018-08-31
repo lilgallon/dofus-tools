8eacc14b3db36ab93d790caea98ca61c985d7fcf
Lilian Gallon <lilian.gallon@etu.u-bordeaux.fr>
2018-08-31 16:22:11 +0200

    add changelog

--- js/index.js
+++ js/index.js
@@ -7,4 +7,15 @@
 $(document).ready(function() {
     // Sidebar toggler
     addSideBarTrigger($(".hamburger"))
+
+    // Spoiler
+    $('.spoiler-text').hide();
+    $('.spoiler-toggle').click(function(){
+        $(this).next().animate({height: 'toggle'});
+        if($(this).html() == '<i class="fas fa-minus"></i> Hide changelog'){
+            $(this).html('<i class="fas fa-plus"></i> Show changelog');
+        }else{
+            $(this).html('<i class="fas fa-minus"></i> Hide changelog');
+        }
+    });
 });
